c61b258a0e343d346837f2803aa17875a835158e
Alex Stevens <zandra630@gmail.com>
2020-01-14 22:22:33 -0600

    Initializes app with db schema and seeds queries

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured TINYINT(1),
+  PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+  ("Double Double Animal Style", FALSE),
+  ("Black and Blue", FALSE),
+  ("Portabello and Swiss", FALSE);
